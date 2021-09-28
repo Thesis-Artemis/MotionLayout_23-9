85bbd7647fa5f5fac4416f9314d943b2cac30d0c
HUONG <lenhatuyenhuong@gmail.com>
2021-09-28 12:40:08 +0700

    commit baby motion

---
+++ app/src/main/java/com/example/babymotion/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.babymotion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.babymotion);
+    }
+}
